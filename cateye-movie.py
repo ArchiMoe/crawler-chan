b8e61f90b4377f9b08f16bfaa630b273e180f7ec
ArchitectMoe <39474589+ArchitectMoe@users.noreply.github.com>
2018-07-31 14:53:54 +0800

    猫眼电影热映口碑榜单爬虫

---
+++ cateye-movie.py
@@ -0,0 +1,40 @@
+import requests,bs4,random
+uapool=[
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.32',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 BIDUBrowser/8.3 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.9.2.1000 Chrome/39.0.2146.0 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36 Core/1.47.277.400 QQBrowser/9.4.7658.400',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 UBrowser/5.6.12150.8 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari/537.36 SE 2.X MetaSr 1.0',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.154 Safari/537.36 LBBROWSER ',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36 TheWorld 7'
+]
+uapic=random.choice(uapool)
+headers={'User-Agent':uapic}
+page=requests.get('http://maoyan.com/board/7',headers=headers)
+html=page.content
+ceobj=bs4.BeautifulSoup(html,'lxml')
+namelist=ceobj.find_all('a',{'title':True,'class':'image-link'})
+online=ceobj.find_all('p',attrs={'class':'releasetime'})
+actor=ceobj.find_all('p',attrs={'class':'star'})
+actlist=[]
+nlist1=[]
+olist=[]
+print('=================猫眼电影数据===================')
+for k in namelist:
+    nlist1.append(k['title'])
+for p in online:
+    olist.append(p.string)
+for act in actor:
+    an=act.string
+    pan=an.replace(' ','')
+    pan=pan.replace('\n','')
+for name,online in zip(nlist1,olist):
+    print('\n影片名称:'+name,online,'领衔'+pan,sep='\n\n',end='\n\n')
+    print('================================================')
+
+
